4f520506374a195bfbe2cd549fc750879950f494
Junho Baik <junhobaik@gmail.com>
2019-09-20 23:35:24 +0900

    global style 파일 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 import './App.scss';
+import './global.scss';
+
 import Header from './Header';
 import Contents from './Contents';
+import Menu from './Menu';
 
 const App = () => {
   return (
     <div id="App">
       <Header />
       <Contents />
+      <Menu />
     </div>
   );
 };
