81f4126f8fbcd8b81e7928f2ab3f03b0674ae0d1
Junho Baik <junhobaik@gmail.com>
2019-09-20 23:35:41 +0900

    Menu 레이아웃 작성

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Icon } from 'semantic-ui-react';
+
+import './index.scss';
+import FeedMenu from './FeedMenu';
 
 const Menu = () => {
   return (
     <div id="Menu">
-      
+      <div className="header">
+        <div className="tab-menu-button-wrap header-btn">Tab</div>
+        <div className="toggle-button-wrap header-btn">
+          <Icon name="angle down" />
+        </div>
+        <div className="feed-menu-button-wrap header-btn">Feed</div>
+      </div>
+      <div className="content">
+        <FeedMenu />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
