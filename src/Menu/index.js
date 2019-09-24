d9fb44400ffc97d6239f775e4958a9e2b106ae2e
Junho Baik <junhobaik@gmail.com>
2019-09-24 22:20:44 +0900

    Menu toggle 관련 기능 구현

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -7,35 +7,95 @@ import TabMenu from './TabMenu';
 
 const Menu = () => {
   const [selectMenu, setSelectMenu] = useState('feed');
+  const [toggleMenu, setToggleMenu] = useState('hide'); // hide, default, extend
+
+  const menuStyle = {
+    bottom: '-13.5rem',
+    height: '15rem',
+    minHeight: '15rem',
+    transition: '0.3s',
+  };
+
+  switch (toggleMenu) {
+    case 'hide':
+      menuStyle.bottom = '-13.5rem';
+      break;
+    case 'default':
+      menuStyle.bottom = '0';
+      break;
+    case 'extend':
+      menuStyle.bottom = '0';
+      menuStyle.height = '60%';
+      break;
+    default:
+      break;
+  }
 
   return (
-    <div id="Menu">
+    <div id="Menu" style={menuStyle}>
       <div className="header">
         <div
           className="tab-menu-button header-btn"
           role="button"
           onClick={() => {
             setSelectMenu('tab');
+            if (toggleMenu === 'hide') setToggleMenu('default');
           }}
         >
-          <span>TAB</span>
+          {selectMenu === 'tab' ? (
+            <span>
+              <strong>TAB</strong>
+            </span>
+          ) : (
+            <span>TAB</span>
+          )}
         </div>
-        <div className="toggle-button header-btn">
-          <Icon name="angle down" />
+
+        {toggleMenu === 'default' ? (
+          <div
+            className="toggle-extend-button header-btn header-half-btn"
+            role="button"
+            onClick={() => {
+              setToggleMenu('extend');
+            }}
+          >
+            <Icon name="angle double up" />
+          </div>
+        ) : null}
+
+        <div
+          className={`toggle-button header-btn ${toggleMenu === 'default' ? 'header-half-btn' : null}`}
+          role="button"
+          onClick={() => {
+            if (toggleMenu === 'hide') {
+              setToggleMenu('default');
+            } else {
+              setToggleMenu('hide');
+            }
+          }}
+        >
+          <Icon name={toggleMenu === 'hide' ? 'angle up' : 'angle down'} />
         </div>
+
         <div
           className="feed-menu-button header-btn"
           role="button"
           onClick={() => {
             setSelectMenu('feed');
+            if (toggleMenu === 'hide') setToggleMenu('default');
           }}
         >
-          <span>FEED</span>
+          {selectMenu === 'feed' ? (
+            <span>
+              <strong>FEED</strong>
+            </span>
+          ) : (
+            <span>FEED</span>
+          )}
         </div>
       </div>
-      <div className="content">
-        {selectMenu === 'feed' ? <FeedMenu /> : <TabMenu />}
-      </div>
+
+      <div className="content">{selectMenu === 'feed' ? <FeedMenu /> : <TabMenu />}</div>
     </div>
   );
 };
