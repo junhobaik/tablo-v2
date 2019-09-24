41779ce6f6b005a4d786d1df733a2635c96d9590
Junho Baik <junhobaik@gmail.com>
2019-09-25 00:24:34 +0900

    Header 구현 및 스타일 변경

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Icon, Button } from 'semantic-ui-react';
 
 import './index.scss';
 
-const Header = () => {
-  return <div id="Header">[Header Component]</div>;
+const Header = ({ selectedWindow, setSelectedWindow }) => {
+  return (
+    <div id="Header">
+      <div className="title">
+        <h1>Tablo v2</h1>
+      </div>
+      <div className="select-content">
+        <Button.Group className="button-group">
+          <Button
+            className={`left ${selectedWindow !== 'feed' ? 'selected' : null}`}
+            onClick={() => {
+              setSelectedWindow('tab');
+            }}
+          >
+            TAB
+          </Button>
+          <Button.Or />
+          <Button
+            className="center"
+            onClick={() => {
+              setSelectedWindow('both');
+            }}
+          >
+            BOTH
+          </Button>
+          <Button.Or />
+          <Button
+            className={`right ${selectedWindow !== 'tab' ? 'selected' : null}`}
+            onClick={() => {
+              setSelectedWindow('feed');
+            }}
+          >
+            FEED
+          </Button>
+        </Button.Group>
+      </div>
+      <div className="setting">
+        <div className="setting-icon-wrap">
+          <Icon name="cog" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
