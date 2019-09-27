e50e4d0a46fab106e6d2b8db86278b6aac3cd9fe
Junho Baik <junhobaik@gmail.com>
2019-09-28 00:13:57 +0900

    redux store 설정

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import 'semantic-ui-css/semantic.min.css';
 
-import App from './src/App';
 import './index.scss';
+import App from './src/App';
+import rootReducer from './src/redux/reducers';
 
-ReactDom.render(<App />, document.querySelector('#root'));
+const preloadedState = {
+  feed: [
+    {
+      url: 'https://junhobaik.github.io/rss',
+      title: 'My blog',
+      category: 'Blog',
+      isHide: false,
+    },
+  ],
+  app: {
+    window: 'both',
+    linkMethod: {
+      tab: 'blank',
+      feed: 'blank',
+    },
+  },
+};
+
+const store = createStore(rootReducer, preloadedState, composeWithDevTools());
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
