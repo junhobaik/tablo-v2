92ec2961ffbed0e51a4ad456accc5b50a6fa52f8
Junho Baik <junhobaik@gmail.com>
2019-09-29 20:28:48 +0900

    Menu, toggle button 관련 코드 개선

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -37,6 +37,52 @@ const Menu = () => {
       break;
   }
 
+  // eslint-disable-next-line no-shadow
+  const createToggleBtnGroup = (isMenuAlwaysOpen, menuStatus) => {
+    const createToggleBtn = (isHalfBtn, iconName, setMenu, key) => {
+      return (
+        <div
+          key={key}
+          className={`toggle-button header-btn ${isHalfBtn ? 'header-half-btn' : null}`}
+          role="button"
+          tabIndex={0}
+          onClick={() => {
+            dispatch(setMenuOpenStatus(setMenu));
+          }}
+        >
+          <Icon name={iconName} />
+        </div>
+      );
+    };
+
+    if (isMenuAlwaysOpen) {
+      // extend / isMenuAlwaysOpen
+      if (menuStatus === 'extend') {
+        const down = createToggleBtn(false, 'angle down', 'default', 'down');
+        return [down];
+      }
+      // default /isMenuAlwaysOpen
+      const extend = createToggleBtn(false, 'angle double up', 'extend', 'extend');
+      return [extend];
+    }
+    // extend
+    if (menuStatus === 'extend') {
+      const down = createToggleBtn(false, 'angle down', 'default', 'down');
+      return [down];
+    }
+    // hide
+    if (menuStatus === 'hide') {
+      const up = createToggleBtn(false, 'angle up', 'default', 'up');
+      return [up];
+    }
+    // default
+    const extend = createToggleBtn(true, 'angle double up', 'extend', 'extend');
+    const down = createToggleBtn(true, 'angle down', 'hide', 'down');
+    return [extend, down];
+  };
+
+  const toggleButtons = createToggleBtnGroup(isMenuAlwaysOpen, menuStatus);
+
   useEffect(() => {
     if (menuStatus === 'hide') {
       if (windowStatus === 'feed') {
@@ -68,33 +114,7 @@ const Menu = () => {
           )}
         </div>
 
-        {menuStatus === 'default' ? (
-          <div
-            className="toggle-extend-button header-btn header-half-btn"
-            role="button"
-            tabIndex={0}
-            onClick={() => {
-              dispatch(setMenuOpenStatus('extend'));
-            }}
-          >
-            <Icon name="angle double up" />
-          </div>
-        ) : null}
-
-        <div
-          className={`toggle-button header-btn ${menuStatus === 'default' ? 'header-half-btn' : null}`}
-          role="button"
-          tabIndex={0}
-          onClick={() => {
-            if (menuStatus === 'hide') {
-              dispatch(setMenuOpenStatus('default'));
-            } else {
-              dispatch(setMenuOpenStatus('hide'));
-            }
-          }}
-        >
-          <Icon name={menuStatus === 'hide' ? 'angle up' : 'angle down'} />
-        </div>
+        {toggleButtons}
 
         <div
           className="feed-menu-button header-btn"
