1913eee6e63a5488521fdaee7a3311d27550f57a
Junho Baik <junhobaik@gmail.com>
2019-09-29 22:33:59 +0900

    FeedMenu 관련 구현 사항
    - 주요 마크업 구현
    - 설정 관련 마크업은 아직 미구현
    - 기타 스타일 구현

--- src/Menu/FeedMenu/index.js
+++ src/Menu/FeedMenu/index.js
@@ -1,9 +1,68 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react';
+import { Icon } from 'semantic-ui-react';
 
 import './index.scss';
 
 const FeedMenu = () => {
-  return <div id="FeedMenu">[FeedMenu Component]</div>;
+  const [isAddFeed, setIsAddFeed] = useState(false);
+  return (
+    <div id="FeedMenu">
+      <div
+        className="add-icon-button"
+        role="button"
+        tabIndex="0"
+        onClick={() => {
+          setIsAddFeed(!isAddFeed);
+        }}
+      >
+        <Icon name={isAddFeed ? 'close' : 'plus'} />
+      </div>
+      <div className="feed-menu-inner">
+        {!isAddFeed ? (
+          <div className="feed-list-wrap">
+            <div className="feed-list">
+              <div className="feed-list-header">
+                <div className="feed-list-title">
+                  <span>Category</span>
+                </div>
+                <div className="feed-list-setting">
+                  <Icon name="eye" />
+                  <Icon name="ellipsis horizontal" />
+                </div>
+              </div>
+              <div className="feed-list-content">
+                <div className="feed">
+                  <div className="feed-visible">
+                    <div className="visible-icon visible"></div>
+                  </div>
+                  <div className="feed-title">
+                    <a href="#">TITLE</a>
+                  </div>
+                  <div className="feed-setting">
+                    <Icon name="ellipsis horizontal" />
+                  </div>
+                </div>
+                <div className="feed">
+                  <div className="feed-visible">
+                    <div className="visible-icon"></div>
+                  </div>
+                  <div className="feed-title">
+                    <a href="#">TITLE</a>
+                  </div>
+                  <div className="feed-setting">
+                    <Icon name="ellipsis horizontal" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="add-feed">addfeed</div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default FeedMenu;
