aa451ada84370ea445ededcfa0693a8235f779cc
Junho Baik <junhobaik@gmail.com>
2019-09-30 01:47:54 +0900

    Setting 컴포넌트 위치 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import './App.scss';
 import './global.scss';
@@ -6,13 +7,16 @@ import './global.scss';
 import Header from './Header';
 import Contents from './Contents';
 import Menu from './Menu';
+import Setting from './Setting';
 
 const App = () => {
+  const { settingInfo } = useSelector(state => state.app);
   return (
     <div id="App">
       <Header />
       <Contents />
       <Menu />
+      <Setting settingInfo={settingInfo} />
     </div>
   );
 };
