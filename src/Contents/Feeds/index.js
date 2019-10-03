d5d3995fac4304fb6f571edc4e0f6c5adf81186a
Junho Baik <junhobaik@gmail.com>
2019-10-04 00:14:30 +0900

    Feeds 컴포넌트 구현

--- src/Contents/Feeds/index.js
+++ src/Contents/Feeds/index.js
@@ -1,44 +1,98 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+/* eslint-disable react/prop-types */
 import RSSParser from 'rss-parser';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import { Icon } from 'semantic-ui-react';
 
 import './index.scss';
 
-const Feeds = () => {
-  const feeds = useSelector(state => state.feed);
-  const [items, setItems] = useState([]);
+class Feeds extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+    };
+  }
 
-  const pipe = (...funcs) => argument => funcs.reduce((acc, func) => func(acc), argument);
+  componentDidMount() {
+    const pipe = (...funcs) => argument => funcs.reduce((acc, func) => func(acc), argument);
 
-  const setItemsState = (requestUrl, feedTitle) => {
-    const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
-    const parser = new RSSParser();
+    const { feeds } = this.props;
 
-    parser.parseURL(CORS_PROXY + requestUrl, (err, feed) => {
-      if (feed) {
-        const feedItems = feed.items;
-        console.log(feedItems);
+    const addFeedItems = async (requestUrl, feedTitle) => {
+      const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+      const parser = new RSSParser();
 
-        // eslint-disable-next-line no-restricted-syntax
-        for (const item of feedItems) {
+      const result = await parser.parseURL(CORS_PROXY + requestUrl, (err, feed) => {
+        for (const item of feed.items) {
           const { title, link, pubDate, contentSnippet } = item;
-          console.log(title);
-          setItems([...items, { title, link, pubDate, contentSnippet, feedTitle }]);
+          this.setState(prevState => ({
+            items: [...prevState.items, { title, link, pubDate, contentSnippet, feedTitle }].sort((a, b) => {
+              const aDate = parseInt(moment(a.pubDate).format('YYYYMMDD'), 10);
+              const bDate = parseInt(moment(b.pubDate).format('YYYYMMDD'), 10);
+              return aDate > bDate ? -1 : 1;
+            }),
+          }));
         }
-      }
+      });
+      return result;
+    };
+
+    const setAllFeedItems = pipe(
+      ...feeds.map(feed => {
+        return () => addFeedItems(feed.url, feed.title);
+      })
+    );
+    setAllFeedItems();
+  }
+
+  render() {
+    const { items } = this.state;
+
+    const itemList = items.map(item => {
+      const { title, link, pubDate, contentSnippet, feedTitle } = item;
+      return (
+        <li className="item" key={link}>
+          <div className="item-inner">
+            <div className="item-header">
+              <div className="title">
+                <a href={link}>
+                  <h3>{title}</h3>
+                </a>
+              </div>
+              <Icon name="cart" />
+            </div>
+            <div className="item-info">
+              <span className="title">{`"${feedTitle}"`}</span>
+              <span className="date">{moment(pubDate).fromNow()}</span>
+            </div>
+            <div className="item-content">
+              <span>{contentSnippet}</span>
+            </div>
+          </div>
+        </li>
+      );
     });
+
+    return (
+      <div id="Feeds">
+        <ul className="item-list">{itemList}</ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    feeds: state.feed,
   };
-
-  // eslint-disable-next-line no-restricted-syntax
-  const setItemsFuncs = feeds.map(feed => {
-    return () => setItemsState(feed.url, feed.title);
-  });
-
-  const setItemPipe = pipe(...setItemsFuncs);
-  setItemPipe();
-
-  return <div id="Feeds">[Feeds Component]</div>;
 };
 
-export default Feeds;
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Feeds);
