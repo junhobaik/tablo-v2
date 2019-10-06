d8b1924fb122d021bd6b1afd24b944dcb5434280
Junho Baik <junhobaik@gmail.com>
2019-10-06 20:52:51 +0900

    tab redux, deleteCartItem 구현

--- src/redux/actions/tab.js
+++ src/redux/actions/tab.js
@@ -1,5 +1,10 @@
 export const ADD_CART_ITEM = 'ADD_CART_ITEM';
+export const DELETE_CART_ITEM = 'DELETE_CART_ITEM';
 
 export const addCartItem = (url, title, description) => {
   return { type: ADD_CART_ITEM, url, title, description };
+};
+
+export const deleteCartItem = link => {
+  return { type: DELETE_CART_ITEM, link };
 };
--- src/redux/reducers/tab.js
+++ src/redux/reducers/tab.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import _ from 'lodash';
 
-import { ADD_CART_ITEM } from '../actions/tab';
+import { ADD_CART_ITEM, DELETE_CART_ITEM } from '../actions/tab';
 
 const tab = (state = [], action) => {
   switch (action.type) {
@@ -13,6 +13,13 @@ const tab = (state = [], action) => {
           { link: action.link, title: action.title, description: action.description.substr(0, 50) },
         ],
       };
+
+    case DELETE_CART_ITEM: {
+      const newState = _.cloneDeep(state);
+      const index = _.findIndex(newState.cart, ['link', action.link]);
+      newState.cart.splice(index, 1);
+      return newState;
+    }
 
     default:
       return state;
