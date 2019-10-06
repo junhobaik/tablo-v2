0fe13cc4478f4645362c911741e70be042d68e26
Junho Baik <junhobaik@gmail.com>
2019-10-06 20:49:15 +0900

    TabMenu, cart 마크업 구현

--- src/Menu/TabMenu/index.js
+++ src/Menu/TabMenu/index.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Icon } from 'semantic-ui-react';
 
 import './index.scss';
 
 const TabMenu = () => {
-  return <div id="TabMenu">[TabMenu Component]</div>;
+  const { cart } = useSelector(state => state.tab);
+  const linkMethod = useSelector(state => state.app.linkMethod.tab);
+  const aTarget = linkMethod === 'blank' ? '_blank' : '_self';
+
+  const cartItems = cart.map(item => {
+    const { link, title, description } = item;
+    return (
+      <li className="cart-item" key={link}>
+        <div className="drag-handle"></div>
+        <div className="item-content">
+          <div className="cart-item-header">
+            <div className="title">
+              <a href={link} target={aTarget}>
+                <h3>{title}</h3>
+              </a>
+            </div>
+            <div className="icon-wrap">
+              <Icon name="cancel" />
+            </div>
+          </div>
+          <div className="cart-item-description">
+            <span>{description}</span>
+          </div>
+        </div>
+      </li>
+    );
+  });
+
+  return (
+    <div id="TabMenu">
+      <div className="tabs"></div>
+      <div className="cart">
+        <div className="title">
+          {/* <h2>Cart</h2> */}
+          <Icon name="cart" />
+        </div>
+        <ul className="cart-item-list">{cartItems}</ul>
+      </div>
+    </div>
+  );
 };
 
 export default TabMenu;
