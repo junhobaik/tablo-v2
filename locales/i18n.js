b460e2f8fa51909593e2df248788c947f0bfe5cb
Junho Baik <junhobaik@gmail.com>
2019-11-02 23:05:25 +0900

    다국어 언어 파일 추가 및 적용
    - SettingModal에 쓰일 언어 작성

--- locales/i18n.js
+++ locales/i18n.js
@@ -2,18 +2,10 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-const resources = {
-  ko: {
-    translation: {
-      test: '테스트',
-    },
-  },
-  en: {
-    translation: {
-      test: 'TEST',
-    },
-  },
-};
+import ko from './locale.ko.json';
+import en from './locale.en.json';
+
+const resources = { ko, en };
 
 i18n
   .use(LanguageDetector)
